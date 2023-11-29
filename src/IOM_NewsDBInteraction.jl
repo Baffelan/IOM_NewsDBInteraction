6c1d5abbdfaeac13560f07e2cb941cccbfd8069a
StirlingSmith <stirlingjamessmith@gmail.com>
2023-11-29 14:50:20 +1300

    messily ported code across

--- src/IOM_NewsDBInteraction.jl
+++ src/IOM_NewsDBInteraction.jl
@@ -1,5 +1,13 @@
 module IOM_NewsDBInteraction
 
-# Write your package code here.
+include("messy.jl")
+
+export query_postgres
+export parse_array
+export load_processed_data
+export add_new_user
+export get_back_connection
+export get_forward_connection
+export user_from_id
 
 end
